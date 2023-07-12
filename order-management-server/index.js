3e64a6dba7065f61d5b0c15c4a1b7b336bc2033c
Navyacb <navyacb95@gmail.com>
2023-07-12 15:17:16 +0530

    update

--- order-management-server/index.js
+++ order-management-server/index.js
@@ -3,12 +3,13 @@ const configDB = require('./config/dbConfig')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const PORT = 3030
-const app=express()
+const app = express()
 configDB()
 app.use(express.json())
 app.use(cors())
 
 const Schema = mongoose.Schema
+
 const menuSchema = new Schema({
     name : {
         type: String,
@@ -35,9 +36,13 @@ const orderSchema = new Schema({
     }
 },{timestamps:true})
 
+const Order = mongoose.model('Order',orderSchema)
+
 app.get('/menu-list',(req,res)=>{
+    console.log('inside')
     Menu.find()
-    .then(response=>{
+    .then((response)=>{
+        console.log(response)
         res.json(response)
     })
     .catch((error)=>{
@@ -53,6 +58,16 @@ app.post('/add-menu',(req,res)=>{
         res.json(response)
     })
     .error((error)=>{
+        res.json(error)
+    })
+})
+
+app.get('/order-list',(req,res)=>{
+    Order.find()
+    .then((response)=>{
+        res.json(response)
+    })
+    .catch((error)=>{
         res.json(error)
     })
 })
